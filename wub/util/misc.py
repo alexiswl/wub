08367e1caa8a469cabbf33fdbc9f37518e01cceb
Alexis Lucattini <alexiswl@student.unimelb.edu.au>
2018-11-24 13:43:15 +1100

    Added merge_pickles function to misc component of wub

--- wub/util/misc.py
+++ wub/util/misc.py
@@ -3,6 +3,8 @@
 
 import pickle
 import os.path
+import sys
+from itertools import chain
 
 
 def get_fname(fname):
@@ -89,3 +91,91 @@ def pickle_dump(obj, fname):
     fh.flush()
     fh.close()
     return fname
+
+
+def merge_pickles(obj_list):
+    """
+
+    :param obj_list: list of imported pickle objects
+    :return: merged_pickles
+    """
+
+    # Ensure all pickles have the same tag
+    if len(set([pickle['tag'] for pickle in obj_list])) > 1:
+        sys.exit("Multiple tags not yet supported")
+
+    # Create new object
+    merged = dict()
+
+    # Set tag
+    merged['tag'] = obj_list[0]['tag']
+
+    # Set read stats
+    # Standard set of keys for each object
+    read_stats_keys = list(obj_list[0]['read_stats'].keys())
+    # Initialise object
+    merged['read_stats'] = {}
+    # Iterate through each key for read_stats
+    for key in read_stats_keys:
+        # The following attributes are summed
+        if key in ['mapped', 'unmapped']:
+            merged['read_stats'][key] = sum([obj['read_stats'][key] for obj in obj_list])
+        # The following attributes are appended
+        else:
+            merged['read_stats'][key] = list(chain(obj['read_stats'][key] for obj in obj_list))
+
+    # Set error stats
+    merged['error_stats'] = {}
+    # First get all keys (from AAA to NNN)
+    all_error_keys = list(set([obj['error_stats'].keys() for obj in obj_list]))
+    # Each AAA is a dict (A, C, G, T, '-' *)
+    all_nucs = ['A', 'C', 'G', 'T', '-', '*']
+    for error_key in all_error_keys:
+        merged['error_stats'][error_key] = {}
+        for nuc in all_nucs:
+            # Get values
+            nuc_value = sum(obj['error_stats'][error_key][nuc]
+                            for obj in obj_list
+                            if error_key in obj['error_stats'].keys()
+                            and nuc in obj['error_stats'][error_key].keys())
+            if not nuc_value == 0:
+                merged['error_stats'][error_key][nuc] = nuc_value
+
+    # Set indel_stats:
+    # Standard for each stats
+    indel_stats_keys = ['insertion_lengths', 'deletion_lengths', 'insertion_composition']
+    all_nucs = ['A', 'C', 'G', 'T']
+    merged['indel_stats'] = {}
+    for indel_key in indel_stats_keys:
+        merged['indel_stats'][indel_key] = {}
+        if indel_key == 'insertion_composition':
+            for nuc in all_nucs:
+                merged['indel_stats'][indel_key][nuc] = sum(obj_list['indel_stats'][indel_key][nuc]
+                                                            for obj in obj_list)
+        else:
+            # Get all values for this key
+            all_insertion_lengths = list(set(obj['indel_stats'][indel_key].keys() for obj in obj_list))
+            # Sum for each insertion length
+            for length in all_insertion_lengths:
+                merged['indel_stats'][indel_key][length] = sum(obj['indel_stats'][indel_key][length]
+                                                               for obj in obj_list
+                                                               if length in obj['indel_stats'][indel_key].keys())
+
+    # Set the base stats
+    # Standard set of keys
+    base_stats_keys = list(obj_list[0]['base_stats'].keys())
+    merged['base_stats'] = {}
+    for key in base_stats_keys:
+        if key not in ['identity', 'accuracy']:
+            merged['base_stats'][key] = sum(obj['base_stats'][key] for obj in obj_list)
+    # Now calculate identity and accuracy stats
+    merged['base_stats']['identity'] = sum(merged['base_stats']['match']) / \
+                                       (sum(merged['base_stats']['match']) +
+                                        sum(merged['base_stats']['mismatch']))
+    merged['base_stats']['accuracy'] = sum(merged['base_stats']['match']) / \
+                                       (sum(merged['base_stats']['match']) +
+                                        sum(merged['base_stats']['mismatch']) +
+                                        sum(merged['base_stats']['insertion'])
+                                        sum(merged['base_stats']['deletion']))
+
+    return merged
